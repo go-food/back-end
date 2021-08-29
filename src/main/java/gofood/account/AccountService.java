97948c0ae8395a4d48ae80949a3cc4da786bfcaf
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-08-29 16:16:50 +0700

    Update AccountService.java

--- src/main/java/gofood/account/AccountService.java
+++ src/main/java/gofood/account/AccountService.java
@@ -1,6 +1,7 @@
 package gofood.account;
 
 import gofood.base.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -8,6 +9,7 @@ import javax.transaction.Transactional;
 @Service
 @Transactional
 public class AccountService extends BaseService<Account> {
+    @Autowired
     protected AccountService(AccountRepository repo) {
         super(repo);
     }
