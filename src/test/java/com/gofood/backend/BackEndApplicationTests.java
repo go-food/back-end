73e89dd281ef37d8bfaafc26ec7ecb6d375648fa
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-08-30 16:03:13 +0700

    Fix test error

--- src/test/java/com/gofood/backend/BackEndApplicationTests.java
+++ src/test/java/com/gofood/backend/BackEndApplicationTests.java
@@ -1,9 +1,10 @@
 package com.gofood.backend;
 
+import gofood.BackEndApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BackEndApplication.class)
 class BackEndApplicationTests {
 
     @Test
