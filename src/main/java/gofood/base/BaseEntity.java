2613f4c62ebf9761a37b94a1eae2c51ef77d0175
Khai Truong <56820749+khaitruong922@users.noreply.github.com>
2021-09-16 19:13:09 +0700

    Update BaseEntity.java

--- src/main/java/gofood/base/BaseEntity.java
+++ src/main/java/gofood/base/BaseEntity.java
@@ -4,13 +4,14 @@ import com.fasterxml.jackson.annotation.JsonView;
 import gofood.serializer.View;
 
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonView(View.General.class)
     private Integer id;
 
